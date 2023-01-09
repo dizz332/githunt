c598b068cc38118c422572acbd62175a6d6a9f72
dizz-pfeffel <dizzgithub.3d0sx@slmail.me>
2023-01-09 18:27:40 +0000

    https://github.com/mdoodm is a beauty

--- githubapi.py
+++ githubapi.py
@@ -1,11 +1,14 @@
-"""
-@ First modified by dizz-pfeffel
-@ Repo ~ https://github.com/dizz-pfeffel/githunt
-"""
-import requests,re
+import requests,json
 
 class getemail:
     def getemail(user):
-        req = requests.get(f"https://api.github.com/users/{user}/events/public")
-        print(re.findall("email", req))
-getemail.getemail(input(b"Username: "))
+        req = requests.get(f"https://api.github.com/users/{user}/events/public").text
+        req = json.loads(req)
+        counter = -1
+        for key in req:
+            counter += 1 # walk through
+            try:
+                print('Email: '+str(req[int(counter)]['payload']['commits'][0]['author']['email'])) # aids walk through
+            except Exception as e:
+                print(end='') # clean up if it can't find any email attached to the repository
+getemail.getemail(input("Username: "))
