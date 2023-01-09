c665ca28a5be43dab1f7f5ab57f0162cd3ba16e7
Jonathan <122307591+mdoodm@users.noreply.github.com>
2023-01-10 05:37:57 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 """
-@ First modified by dizz-pfeffel
+@ 10-01-2023 modified by mdoodm
++ cleaned up codebase
+
 """
-import requests
+import requests,json
+
+def get_email(user):
+    req = requests.get(f"https://api.github.com/users/{user}/events/public").text #
+    req = json.loads(req)
+    counter = -1 # may need to tinker with this
+    for key in req: # most likely a better way to do this 
+        counter += 1 # walk through
+        try:
+            print('Email: '+str(req[int(counter)]['payload']['commits'][0]['author']['email'])) # inefficient walk through
+        except Exception as e: # need to address this at a later date
+            print(end='') # clean up if it can't find any email attached to the repository
+            
+if __name__ == __main__:
+    user = str(input('Github Username\n:')) # enforcing string
+    get_email(user)
