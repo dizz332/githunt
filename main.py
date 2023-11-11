d36ff1a05f79f82f7b794e686f0348f668102e06
dizzle <112734640+dizz332@users.noreply.github.com>
2023-11-11 00:00:06 +0000

    Update main.py
    unfinished but will be done soon

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-"""
-@ 10-01-2023 modified by mdoodm
-Edit #1
-+ removed offline environment testing on line 19
-
-"""
 import requests,json
 
+# old
+"""
 def get_email(user):
     req = requests.get(f"https://api.github.com/users/{user}/events/public").text #
     req = json.loads(req)
@@ -19,3 +15,30 @@ def get_email(user):
             
 user = str(input('Github Username\n:')) # enforcing string
 get_email(user)
+"""
+
+# new
+def getEmail(user):
+    headers = {
+    "Accept": "application/vnd.github+json",
+    "Authorization": "Bearer <YOUR-TOKEN>",
+    "X-GitHub-Api-Version": "2022-11-28"
+    }
+
+    data = {
+    "blog": "https://github.com/blog",
+    "name": "monalisa octocat"
+    }
+
+    r = requests.patch(
+    "https://api.github.com/user", 
+    headers=headers,
+    json=data
+    )
+    return r.text
+    
+if __name__ == "__main__":
+    user = str(input('Github Username\n:')) # enforcing string
+    print(getEmail(user))
+    
+# this is insanely unfinished
